acbe46d60dfcd2a3753d2b655ca3e74262f3aad1
N0z1r0h <ntge31@gmail.com>
2025-04-03 17:54:10 -0400

    Change Occurences

--- src/client/java/n0z1r0h/vb/VibrantLandsClient.java
+++ src/client/java/n0z1r0h/vb/VibrantLandsClient.java
@@ -2,7 +2,7 @@ package n0z1r0h.vb;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class VibrantLandsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/n0z1r0h/vb/registries/ItemRegistry.java
+++ src/main/java/n0z1r0h/vb/registries/ItemRegistry.java
@@ -1,5 +1,18 @@
 package n0z1r0h.vb.registries;
 
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
 public class ItemRegistry {
-    
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(VibrantLands.MOD_ID, name));
+        Item item = itemFactory.apply(settings.registryKey(key));
+        Registry.register(Registries.ITEM, key, item);
+        return item;
+    }
 }
