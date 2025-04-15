f81950a7a30bbb76c4a380e7254dabab90d3cda4
N0z1r0h <ntge31@gmail.com>
2025-04-15 19:39:39 -0400

    Create BoarEntity.java

---
+++ src/main/java/n0z1r0h/vb/entity/BoarEntity.java
@@ -0,0 +1,53 @@
+package n0z1r0h.vb.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.Angerable;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+public class BoarEntity extends AnimalEntity implements Angerable {
+    public BoarEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    public boolean isBreedingItem(ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public @Nullable PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return null;
+    }
+
+    @Override
+    public int getAngerTime() {
+        return 0;
+    }
+
+    @Override
+    public void setAngerTime(int angerTime) {
+
+    }
+
+    @Override
+    public @Nullable UUID getAngryAt() {
+        return null;
+    }
+
+    @Override
+    public void setAngryAt(@Nullable UUID angryAt) {
+
+    }
+
+    @Override
+    public void chooseRandomAngerTime() {
+
+    }
+}
